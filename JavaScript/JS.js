bf332f492ec973a9617434329a1797a4e69b4355
Lara <48735715+Lara-Lindsay@users.noreply.github.com>
2019-05-03 11:18:23 -0300

    Add files via upload

---
+++ JavaScript/JS.js
@@ -0,0 +1,16 @@
+var audio = new Audio ();
+i=0;
+var playlist = new Array('music/Will He.mp3', 'music/Knee Socks.mp3', 'music/Pay The Man.mp3', 'music/Tranquility Base Hotel Casino.mp3');
+
+function play() {
+    document.getElementById('player').play()
+    
+}
+
+function pause() {
+    document.getElementById('player').pause()
+    
+}
+
+
+
